c678c227443ad0e9a6b39d83fdc9a12b1a9d2f5f
Artem <gpsqaz1@gmail.com>
2024-05-07 16:56:08 +0700

    Update Counter.cs

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -19,7 +19,7 @@ public class Counter : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Count += 1;
-        CounterText.text = "Count : " + Count;
+        CounterText.text = "Яиц: " + Count;
 
         if (other.gameObject)
         {
