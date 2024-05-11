f56d5c958fe5860d9e814a6ee970239457644bde
Artem <gpsqaz1@gmail.com>
2024-05-11 16:36:16 +0700

    Update DetectBrokenEgg.cs
    готовый удалятель яиц + детект падающих

--- Assets/Scripts/DetectBrokenEgg.cs
+++ Assets/Scripts/DetectBrokenEgg.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class DetectBrokenEgg : MonoBehaviour
 {
-    [SerializeField] private GameObject lvlLose;
-    private int Count = 0;
-
-
-    private void Start()
+    [SerializeField] private GameObject lvlLose;   
+    public static int Count;
+   
+    public void Start()
     {
-        Count = 0;
+        Count = 0;       
     }
 
     public void OnTriggerEnter(Collider other)
     {
         Count += 1;
 
-        if (Count == 5)
+        if (Count >= 5)
         {
             lvlLose.SetActive(true);
             Spawner spawner = FindObjectOfType<Spawner>();
